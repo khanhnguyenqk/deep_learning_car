9192bfa7be537cc47d8b0413496e853ea3498deb
Khanh Nguyen <khanhnguyen.qk@gmail.com>
2019-07-13 20:54:26 -0700

    Implement random mutation for neural network

--- deepcar/neural_network/nn.py
+++ deepcar/neural_network/nn.py
@@ -1,5 +1,7 @@
 from typing import List
 import numpy as np
+from itertools import accumulate
+import random
 
 class NN:
     def __init__(self, x_cnt:int, y_cnt:int, hidden_cnts:List[int]):
@@ -9,18 +11,21 @@ class NN:
 
         if len(hidden_cnts) < 1:
             raise Exception('Need to have at least 1 hidden layer.')
-
-        # random weights and biases
-        np.random.seed()
-
+            
         self.hiddens = []
-        random_scale = 1/x_cnt**.5
-        layer = np.random.normal(scale=random_scale, size=(x_cnt+1, hidden_cnts[0]))
+
+    def randomize_weights_biases(self, seed=None):
+        if seed:
+            np.random.seed(seed)
+        else:
+            np.random.seed()
+        random_scale = 1/self.x_cnt**.5
+        layer = np.random.normal(scale=random_scale, size=(self.x_cnt+1, self.hidden_cnts[0]))
         self.hiddens.append(layer)
-        for i in range(1, len(hidden_cnts)):
-            layer = np.random.normal(scale=random_scale, size=(hidden_cnts[i-1]+1, hidden_cnts[i]))
+        for i in range(1, len(self.hidden_cnts)):
+            layer = np.random.normal(scale=random_scale, size=(self.hidden_cnts[i-1]+1, self.hidden_cnts[i]))
             self.hiddens.append(layer)
-        layer = np.random.normal(scale=random_scale, size=(hidden_cnts[-1]+1, y_cnt))
+        layer = np.random.normal(scale=random_scale, size=(self.hidden_cnts[-1]+1, self.y_cnt))
         self.hiddens.append(layer)
 
     def sigmoid(self, x):
@@ -34,3 +39,84 @@ class NN:
             output = np.append(output, [1])
             output = self.sigmoid(np.dot(output, hidden))
         return output.tolist()
+
+    def deep_copy(self, seed=None):
+        '''
+        Return a deep copy of self
+        '''
+        copy = NN(self.x_cnt, self.y_cnt, list(self.hidden_cnts))
+        copy.hiddens = [np.copy(h) for h in self.hiddens]
+        return copy
+
+    def mutate_randomly(self):
+        '''
+        Randomly mutate weights and biases, inplace.
+        '''
+        probs = [0.02] * 4
+        mutation_functions = [NN.flip, NN.rand, NN.rand_increase_pct, NN.rand_deduct_pct]
+        NN._mutate_nparray_with_probs(self.hiddens, probs, mutation_functions)
+
+    @staticmethod
+    def _mutate_nparray_with_probs(a, probs:List[float], mutation_functions, seed=None):
+        '''
+        Similar to mutate_with_probs, but do it on each element of a numpy array. Notice that at each element, the probability is recalculated.
+
+        Method is inplace
+        '''
+        with np.nditer(a, op_flags=['readwrite']) as it:
+            for x in it:
+                x[...] = NN.mutate_with_probs(x, probs, mutation_functions, seed)
+
+    @staticmethod
+    def mutate_with_probs(x, probs:List[float], mutation_functions, seed=None):
+        '''
+        Apply a mutation function on x according to the function's probability
+
+        INPUT
+            x (float): the number to apply mutation function on
+            probs (List[float]): list of propbabilities of mutation_functions. 
+                Has to have the same length with list of mutation_functions. If sum of probs < 1, return x with the probability of 1 - sum(probs)
+            mutation_functions(List[functions]): list of functions that take a float as an argument and return a float.
+        OUTPUT
+            float: mutated of input
+        '''
+        n = len(probs)
+        if n != len(mutation_functions):
+            raise Exception('len(probs) != len(mutation_functions)')
+        accum_probs = list(accumulate(probs))
+
+        random.seed(seed)
+        r = random.random()
+        for i, p in enumerate(accum_probs):
+            if r <= p:
+                return mutation_functions[i](x)
+        return x
+
+    flip = lambda x:-x
+    rand = lambda x:x - x + random.random() # not redundant, use this to apply on np.array
+    rand_increase_pct = lambda x:x*(1 + random.random())
+    rand_deduct_pct = lambda x:x*random.random()
+
+def _test_forward_no_exception():
+    nn = NN(5, 2, [10, 10])
+    nn.randomize_weights_biases(42)
+    xs = np.random.normal(size=5).tolist()
+    ys = nn.forward(xs)
+    print(ys)
+
+def _test_mutate_with_probs():
+    input = [1] * 10
+    mutate = lambda x: NN.mutate_with_probs(x, [0.2] * 4, [NN.flip, NN.rand, NN.rand_increase_pct, NN.rand_deduct_pct])
+    output = map(mutate, input)
+    print(list(output))
+
+def _test_mutate_nparray_with_probs():
+    a = np.array([float(x) for x in range(20)]).reshape((4, 5))
+    f = lambda x:x + 1
+    v = np.vectorize(NN.mutate_with_probs)
+    NN._mutate_nparray_with_probs(a, [0.2] * 4, [NN.flip, NN.rand, NN.rand_increase_pct, NN.rand_deduct_pct])
+    print(a)
+
+if __name__ == '__main__':
+    _test_forward_no_exception()
+    _test_mutate_with_probs()
